a47473592f7e8c4d1334dad53163e21c3ed314d4
StealthC <cacefmail@gmail.com>
2025-01-30 15:35:15 +0000

    lib start

---
+++ packages/kid-util/jest.config.ts
@@ -0,0 +1,14 @@
+import { createDefaultEsmPreset, type JestConfigWithTsJest } from 'ts-jest';
+
+const preset = createDefaultEsmPreset({
+  // A separate TypeScript configuration file for Jest.
+  tsconfig: './tsconfig.jest.json',
+});
+
+const jestConfig: JestConfigWithTsJest = {
+  ...preset,
+
+  // Other options.
+};
+
+export default jestConfig;
---
+++ packages/kid-util/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Sum two numbers
+ * @param a number
+ * @param b number
+ * @returns number
+ */
+export function sum(a: number, b: number): number {
+    return a + b;
+}
